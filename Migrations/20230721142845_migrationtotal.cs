878a68b046a0dacd503735cb56ed243a0cdf0382
Clara <91093218+ClaraRazzetto@users.noreply.github.com>
2023-07-21 11:48:23 -0300

    migracion product status

---
+++ Migrations/20230721142845_migrationtotal.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shop.API.Migrations
+{
+    public partial class migrationtotal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Products");
+        }
+    }
+}
